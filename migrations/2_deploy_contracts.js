a22beb85da7e31dc5cd0ae577a47655e0ddc79e4
scion <scion@scion-mbp.local>
2018-11-23 10:48:09 +0900

    truffle project is removed

--- migrations/2_deploy_contracts.js
+++
@@ -1,9 +0,0 @@
-// const Greeter = artifacts.require('./Greeter.sol');
-// const EcRecover = artifacts.require('./EcRecover.sol');
-const KeyValue = artifacts.require('./KeyValue.sol');
-
-module.exports = function (deployer, network, accounts) {
-  // deployer.deploy(Greeter);
-  // deployer.deploy(EcRecover);
-  deployer.deploy(KeyValue);
-};
